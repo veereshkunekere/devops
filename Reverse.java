943168e83ae705672b8f13c3d63a92523041c59e
veereshkunekere <veeresh312004@gmail.com>
2026-06-28 15:07:14 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,6 +1,8 @@
 public class Reverse {
     public static void main(String[] args){
      int n= Integer.parseInt(args[0]);
-      System.out.println(n);
+        for(int i=n;i>0;i--){
+                  System.out.println(i);
+        }
     }
 }
